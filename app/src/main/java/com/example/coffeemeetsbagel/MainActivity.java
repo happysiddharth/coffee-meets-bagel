b96fa56a7f42f915b54a750dcd1be52e36e4557b
unknown <siddharth9k9@gmail.com>
2020-12-30 00:46:14 +0530

    activies added

--- app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
+++ app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button fb_login_btn,termnConditionBtn;
+    private Button fb_login_btn,termnConditionBtn,continueWithPhoneBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +29,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(fbLoginIntent);
             }
         });
+        continueWithPhoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent continueWithPhoneNumber = new Intent(getApplicationContext(),SettlePeriod.class);
+                startActivity(continueWithPhoneNumber);
+            }
+        });
     }
 
-    private void init(){
+        private void init(){
         fb_login_btn = findViewById(R.id.fb_login_btn);
         termnConditionBtn = findViewById(R.id.termnConditionBtn);
+        continueWithPhoneBtn = findViewById(R.id.continueWithPhoneBtn);
     }
 }
