e388a928932a7378ad63649a3c01300d34da0a08
@Aditya rout <adityaroutgood@gmail.com>
2020-12-29 20:02:58 +0530

    enter phone no page

---
+++ app/src/main/java/com/example/coffeemeetsbagel/EnterPhoneNoToSignup.java
@@ -0,0 +1,14 @@
+package com.example.coffeemeetsbagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterPhoneNoToSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_phone_no_to_signup);
+    }
+}
