7826d9830612e37ce7d05cb41abf54271e23fbe3
unknown <siddharth9k9@gmail.com>
2020-12-29 15:09:19 +0530

    homepage done by siddharth

--- app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
+++ app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.coffeemeetsbagel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button fb_login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        fb_login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Tag","fb button clicked");
+            }
+        });
+    }
+    private void init(){
+        fb_login_btn = findViewById(R.id.fb_login_btn);
     }
 }
