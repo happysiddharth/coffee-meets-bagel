a74c0732e408459dd349808e13b29b4ea17f2269
unknown <siddharth9k9@gmail.com>
2020-12-29 20:47:13 +0530

    intro step 1 added

---
+++ app/src/main/java/com/example/coffeemeetsbagel/IntroStep1.java
@@ -0,0 +1,14 @@
+package com.example.coffeemeetsbagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_step1);
+    }
+}
