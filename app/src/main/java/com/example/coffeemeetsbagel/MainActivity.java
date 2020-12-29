74b715f9750ef61262e3bb7fd0e7747afbfcdffd
unknown <siddharth9k9@gmail.com>
2020-12-29 17:30:13 +0530

    term and services page added

--- app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
+++ app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
@@ -2,26 +2,29 @@ package com.example.coffeemeetsbagel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button fb_login_btn;
+    private Button fb_login_btn,termnConditionBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        fb_login_btn.setOnClickListener(new View.OnClickListener() {
+        termnConditionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Tag","fb button clicked");
+                Intent viewTermNCondition = new Intent(getApplicationContext(),TermsAndPrivacy.class);
+                startActivity(viewTermNCondition);
             }
         });
     }
     private void init(){
         fb_login_btn = findViewById(R.id.fb_login_btn);
+        termnConditionBtn = findViewById(R.id.termnConditionBtn);
     }
 }
