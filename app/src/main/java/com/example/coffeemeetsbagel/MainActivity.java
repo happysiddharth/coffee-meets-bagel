9ad438e4a527ebb2f984781c1a832f9fb78b069c
Nishant Rishab <imreprisal@gmail.com>
2020-12-30 11:34:03 +0530

    Commit by Nishant

--- app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
+++ app/src/main/java/com/example/coffeemeetsbagel/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button fb_login_btn,termnConditionBtn;
+    private Button fb_login_btn,termnConditionBtn,continueWithPhoneBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +22,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(viewTermNCondition);
             }
         });
+        fb_login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fbLoginIntent = new Intent(getApplicationContext(),IntroStep1.class);
+                startActivity(fbLoginIntent);
+            }
+        });
+        continueWithPhoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent continueWithPhoneNumber = new Intent(getApplicationContext(),SettlePeriod.class);
+                startActivity(continueWithPhoneNumber);
+            }
+        });
     }
-    private void init(){
+
+        private void init(){
         fb_login_btn = findViewById(R.id.fb_login_btn);
         termnConditionBtn = findViewById(R.id.termnConditionBtn);
+        continueWithPhoneBtn = findViewById(R.id.continueWithPhoneBtn);
     }
 }
