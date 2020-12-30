3296e9e5759764f14b4f84768e4e22a0e319e20c
@Aditya rout <adityaroutgood@gmail.com>
2020-12-30 20:11:08 +0530

    upload and date page

---
+++ app/src/main/java/com/example/coffeemeetsbagel/cmbDaters.java
@@ -0,0 +1,14 @@
+package com.example.coffeemeetsbagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cmbDaters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cmb_daters);
+    }
+}
