28146e2861b6256fbef2578e7c565255fc1abf07
unknown <siddharth9k9@gmail.com>
2020-12-30 22:04:18 +0530

    layout modified

--- app/src/main/java/com/example/coffeemeetsbagel/SettlePeriod.java
+++ app/src/main/java/com/example/coffeemeetsbagel/SettlePeriod.java
@@ -89,6 +89,8 @@ public class SettlePeriod extends AppCompatActivity {
             this.currentActiveRadioButton = currentRadioButtonId;
             this.previousActiveRadioButton = currentRadioButtonId;
             nextBtn.setBackground(getDrawable(R.drawable.next_btn_enable));
+            nextBtn.setTextColor(getResources().getColor(R.color.white));
+
         }
 
     }
