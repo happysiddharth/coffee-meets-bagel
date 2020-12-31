e6a927902acd8c1431d57893deddd6f3aa6e3d73
@Aditya rout <adityaroutgood@gmail.com>
2020-12-31 15:54:34 +0530

    shopWallet page

---
+++ app/src/main/java/com/example/coffeemeetsbagel/Shop_Wallet.java
@@ -0,0 +1,14 @@
+package com.example.coffeemeetsbagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_Wallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop__wallet);
+    }
+}
