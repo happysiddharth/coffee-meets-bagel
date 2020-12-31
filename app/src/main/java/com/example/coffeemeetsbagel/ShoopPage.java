5eabc3b232ea6f5e8fffe5df288002efecb2876b
@Aditya rout <adityaroutgood@gmail.com>
2020-12-31 12:30:09 +0530

    Shop beans page

---
+++ app/src/main/java/com/example/coffeemeetsbagel/ShoopPage.java
@@ -0,0 +1,14 @@
+package com.example.coffeemeetsbagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoopPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoop_page);
+    }
+}
