9879028a5a36e78ba43ae995f544e0acbc04fbaf
unknown <siddharth9k9@gmail.com>
2021-01-02 11:02:09 +0530

    profile page

--- app/src/main/java/com/example/coffeemeetsbagel/ProfilePage.java
+++ app/src/main/java/com/example/coffeemeetsbagel/ProfilePage.java
@@ -3,12 +3,24 @@ package com.example.coffeemeetsbagel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfilePage extends AppCompatActivity {
-
+    private Button backFromProfilePage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        init();
+        backFromProfilePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void init(){
+        backFromProfilePage = findViewById(R.id.backFromProfilePage);
     }
 }
